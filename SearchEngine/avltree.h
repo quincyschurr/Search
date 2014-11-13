f93ee0199179d3867aa5d65658688cfd7606becd
Quincy Schurr <qschurr@smu.edu>
2014-11-13 10:36:19 -0600

    added more functionality to AVLTree, linker error

--- SearchEngine/avltree.h
+++ SearchEngine/avltree.h
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+//good website for the traversal
+//http://www.sanfoundry.com/cpp-program-implement-avl-trees/
+
 class AVLTree
 {
 private:
@@ -15,13 +18,23 @@ private:
         string element;
         AVLNode* left;
         AVLNode* right;
-        AVLNode(string word, AVLNode* l, AVLNode* r, int h) : element(word), left(l), right(r), height(h) {}
+        //AVLNode(string word, AVLNode* l, AVLNode* r, int h) : element(word), left(l), right(r), height(h) {}
+        AVLNode(string word, AVLNode* l, AVLNode* r) : element(word), left(l), right(r) {}
         ~AVLNode(){}
 
     };
+
+    AVLNode* root;
 public:
-    ~AVLTree();
+    void insert(string word, AVLNode*& t);
+    int height(AVLNode* t);
+    void rotateWithLeftChild(AVLNode*& alpha);
+    void doubleWithLeftChild(AVLNode*& t);
+    void rotateWithRightChild(AVLNode*& alpha);
+    void doubleWithRightChild(AVLNode*& t);
+
     AVLTree();
+    ~AVLTree();
 };
 
 #endif // AVLTREE_H
